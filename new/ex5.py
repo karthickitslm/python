e29e944bcb2d34e558e49943c8caa5f202dd1868
Karthick Santharam <karthickitslm@gmail.com>
2023-11-16 19:34:10 +0530

    exercisenumber5

---
+++ new/ex5.py
@@ -0,0 +1,21 @@
+my_name = 'Karthick S Santharam'
+my_age = 36
+my_height = 163 # Centimeters
+my_weight = 68 # Kilograms
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cms tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+
+# This line is tricky, try to get it eactly right
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
