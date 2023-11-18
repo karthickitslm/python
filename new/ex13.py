cae780976e7832b78b08df66814e2d7c121644bd
Karthick Santharam <karthickitslm@gmail.com>
2023-11-18 22:47:22 +0530

    exercisenumber13

---
+++ new/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = argv
+
+print("The Script is call:", script)
+print("Your First Variable is:", first)
+print("Your Second Variable is:", second)
+print("Your Third Variable is::", third)
