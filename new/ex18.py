a8342afbba6604fc5b5cfd0a3ef66145727879a3
Karthick Santharam <karthickitslm@gmail.com>
2023-11-20 13:58:46 +0530

    Names, Variables, Code, Functions

---
+++ new/ex18.py
@@ -0,0 +1,26 @@
+# this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# Okay, that * args is actually pointless, we can just do this.
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+# This Just takes one Argument
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# This one takes no Argument
+
+def print_none():
+    print("I got nothing'.")
+
+
+print_two("Karthick","Santharam")
+print_two_again("Karthick","Santharam")
+print_one("First!")
+print_none()
