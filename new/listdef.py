3cfde70da28ce38ceb30d8ece44dc98e0f6ddb87
karthickitslm <karthickitslm@gmail.com>
2024-07-09 20:20:59 +0530

    List with Examples and few operations on List

---
+++ new/listdef.py
@@ -0,0 +1,16 @@
+# Defining List with values as Numbers in Python 
+numbers = [1, 2, 3, 4, 5, 6, 6, 2, 3, 4, 100]
+# Defining List with values as characters in Python
+fruits = ["Apple", "Basket", "Choclate", "Database", "Elephant", "Fantastic"]
+# Defining Boolean as Values in List
+sample = [True, False, False, True]
+# Printing the Values of Defined List
+print(numbers)
+print(fruits)
+print(sample)
+# Finding the total values of numbers defined in the list
+print(sum(numbers))
+# Finding the Max Value in the list
+print(max(numbers))
+# Sorting the list in the Ascending Manner
+print(sorted(numbers))
