354d182300a1bd78cc3297cf448cbcc5eb933aef
karthickitslm <karthickitslm@gmail.com>
2024-07-12 16:12:53 +0530

    Soting List

---
+++ new/mergelist.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+# Merging 2 List using + Operator
+list3 = list1 + list2
+print(list3)
+
+list4 = [2, 4, 6, 8, 10]
+list5 = [11, 31, 51, 71, 91]
+list6 = list4 + list5
+# Sorting list in Ascending Order.
+list7 = sorted(list6)
+print(list7)
