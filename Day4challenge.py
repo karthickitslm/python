e1e45940b8254ec60d12dfdb56e277eb3c332082
karthickitslm <karthickitslm@gmail.com>
2025-12-15 16:02:21 +0530

    Day4

---
+++ Day4challenge.py
@@ -0,0 +1,6 @@
+number= (input("Enter a Number for which the multiplication table to be generated"))
+x = input("Enter the range of the multiplication")
+y = int(x)
+z = int(number)
+for i in range(1, y+1):
+    print(i*z)
